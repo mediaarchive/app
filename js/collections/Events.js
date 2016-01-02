d889e804d926a5404aeb7689a66cad5307f566a1
atnartur <atnartur2012@gmail.com>
2016-01-03 01:32:32 +0300

    + лейблы по месяцам в списке мероприятий

--- js/collections/Events.js
+++ js/collections/Events.js
@@ -17,8 +17,10 @@ class EventsCollection extends MK.Array{
             
         console.log('events collection: after recreate');
         
-        $('#events_content').liveFilter('#search', 'li');
         this.sort();
+        this.update_labels();
+        
+        $('#events_content').liveFilter('#search', 'li:not(.time-label)');
     }
     sort(){
         console.log('sorting');
@@ -35,5 +37,37 @@ class EventsCollection extends MK.Array{
         $.each(items, function(i, li){
             ul.append(li);
         });
+    }
+    update_labels(){
+        var labels = {};
+        $('#events_content .event').each(function(){
+            var label = $(this).data('date-label');
+            
+            if (typeof labels[label] === 'undefined') 
+                labels[label] = true;
+        });
+        
+        for (var key in labels) {
+            var month = key.substr(4);
+            var year = key.substr(0, 4);
+            
+            month = month
+                .replace('01', 'январь')
+                .replace('02', 'февраль')
+                .replace('03', 'март')
+                .replace('04', 'апрель')
+                .replace('05', 'май')
+                .replace('06', 'июнь')
+                .replace('07', 'июль')
+                .replace('08', 'август')
+                .replace('09', 'сентябрь')
+                .replace('10', 'октябрь')
+                .replace('11', 'ноябрь')
+                .replace('12', 'декабрь')
+            
+            $('#events_content .event[data-date-label='+key+']:eq(0)').before('<li class="time-label">'+
+                  '<span class="bg-red">' + month + ' ' + year + '</span>'+
+            '</li>')
+        }
     }
 };
