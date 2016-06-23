256d58e9e744eb5daa127c8320314928e75db204
atnartur <i@atnartur.ru>
2016-06-23 15:46:24 +0300

    + Запуск сайта

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var package = require('../package.json')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	console.log(package)
+    res.render('index', {
+    	assets: package.assets,
+    });
 });
 
 module.exports = router;
